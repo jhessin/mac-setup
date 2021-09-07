5def5c2e50e097fdda32f615f2ae942c3f7746a1
Jim Hessin <jhessin@gmail.com>
2021-09-06 21:15:50 -0600

    fixed nvm issue

--- setup.sh
+++ setup.sh
@@ -81,6 +81,9 @@ python3 -m pip install $(cat ./pip.packages) --user
 
 # install nvm and node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 nvm use --lts
 
