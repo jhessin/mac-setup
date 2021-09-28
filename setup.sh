4b0621fa1113ec0b1ccf917e3245e6e00daca4c2
Jim Hessin <jhessin@gmail.com>
2021-09-28 16:28:34 -0600

    using rvm and ruby 3

--- setup.sh
+++ setup.sh
@@ -76,9 +76,9 @@ find ~/.gnupg -type f -exec chmod 600 {} \;
 find ~/.gnupg -type d -exec chmod 700 {} \;
 
 # install ruby and gems
-#gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-#curl -sSL https://get.rvm.io | bash -s stable
-#rvm install ruby-head
+gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+rvm install ruby-3
 sudo gem install $(cat ./gem.packages)
 
 # install fzf
